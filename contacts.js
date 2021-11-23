bb29fa17604fef85f4a0364e2be2499a1b06a3b4
AlexTvardGit <alex.tvard.git@gmail.com>
2021-11-23 23:11:35 +0200

    fix
    fixes:
    - res variable
    - remove console.log
    - add gitignore

--- contacts.js
+++ contacts.js
@@ -1,20 +1,23 @@
 const path = require("path");
 const fs = require("fs").promises;
 const generateUniqueId = require("generate-unique-id");
-let res = {};
 
 const contactsPath = path.resolve("./db/contacts.json");
 
 async function listContacts() {
     const contacts = await fs.readFile(contactsPath, "utf8");
+    let res = {};
+
     res = JSON.parse(contacts);
-    console.table(JSON.parse(contacts));
-    return JSON.parse(contacts);
+    console.table(res);
+    return res;
 }
 
 
 async function getContactById(contactId) {
     const parsedContacts = await listContacts();
+    let res = {};
+
     res = parsedContacts.find((contact) => contact.id === Number(contactId));
     console.log(res);
     return res;
@@ -25,12 +28,14 @@ async function removeContact(contactId) {
     const updatedContacts = JSON.stringify(
         parsedContacts.filter((contact) => contact.id !== contactId)
     );
-    console.log(`Contact, with contactId ${contactId}, deleted`);
 
-    return fs.writeFile(contactsPath, updatedContacts, "utf8");
+    return fs.writeFile(contactsPath, JSON.stringify(updatedContacts), "utf8");
 }
 
 async function addContact(name, email, phone) {
+    if (name === undefined || email === undefined || phone === undefined)
+        return console.error("Please, fill all arguments");
+
     const contacts = await listContacts();
     const id = Number(generateUniqueId({ length: 5, useLetters: false }));
     const newContact = { id, name, email, phone };
